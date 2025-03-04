b5f33808ce1f9496ce085754ae6f8a8e3ebc579e
baxodirov2006 <baxodirovulugbek593@gmail.com>
2025-03-04 14:25:01 +0500

    OrderServiseDone

---
+++ AstroPharm.Service/DTOs/Order/OrderForUpdateDto.cs
@@ -0,0 +1,10 @@
+using AstroPharm.Domain.Entities;
+
+public class OrderForUpdateDto
+{
+    public long UserId { get; set; }
+    public User User { get; set; }
+    public long MedicationId { get; set; }
+    public Medication Medication { get; set; }
+    public long TotalAmount { get; set; }
+}
