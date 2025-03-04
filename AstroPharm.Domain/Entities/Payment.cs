d9724620716d914cd36c7aa0c7930575771791ba
dkar1mov03 <dkarimov049@gmail.com>
2025-03-04 16:29:01 +0500

    Changed all relation

---
+++ AstroPharm.Domain/Entities/Payment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstroPharm.Domain.Entities
+{
+    internal class Payment
+    {
+    }
+}
