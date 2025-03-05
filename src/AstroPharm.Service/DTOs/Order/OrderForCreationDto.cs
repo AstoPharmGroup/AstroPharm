62bf522db1a86240790e634eff7163d1774a0808
baxodirov2006 <baxodirovulugbek593@gmail.com>
2025-03-05 22:13:33 +0500

    OrderReWritenFully

--- src/AstroPharm.Service/DTOs/Order/OrderForCreationDto.cs
+++ src/AstroPharm.Service/DTOs/Order/OrderForCreationDto.cs
@@ -3,8 +3,7 @@ using AstroPharm.Domain.Entities;
 public class OrderForCreationDto
 {
     public long UserId { get; set; }
-    public User User { get; set; }
-    public long MedicationId { get; set; }
-    public Medication Medication { get; set; }
+    public virtual User User { get; set; }
+    public DateTime OrderDate { get; set; }
     public long TotalAmount { get; set; }
 }
